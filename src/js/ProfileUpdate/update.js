bf7267ed233cf1ce0a6bf10c783c9392a86f586d
gns0314 <gns0314@naver.com>
2023-08-28 23:00:01 +0900

    feat: 회원정보 수정 구현

---
+++ src/js/ProfileUpdate/update.js
@@ -0,0 +1,51 @@
+import { apiGet, apiPut } from '../utils/fetchAPI.js';
+import { URL } from '../data/index.js';
+import { getSessionStorage } from '../utils/storage.js';
+
+const $updatebtn = document.querySelector("#update-btn");
+
+
+async function fetchData() {
+    try {
+        const token = getSessionStorage("token");
+
+        const data = await apiGet(URL.buyerUpdateURL, token);
+        const nicknameField = document.getElementById("nickname");
+        const numberField = document.getElementById("number");
+        const shippingAddressField = document.getElementById("shipping_address");
+
+        nicknameField.value = data.nickname;
+        numberField.value = data.number;
+        shippingAddressField.value = data.shipping_address;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+async function update(event) {
+    event.preventDefault();
+
+    try {
+        const token = getSessionStorage("token");
+        const nicknameField = document.getElementById("nickname");
+        const numberField = document.getElementById("number");
+        const shippingAddressField = document.getElementById("shipping_address");
+
+        await apiPut(URL.buyerUpdateURL, {
+            nickname: nicknameField.value,
+            number: numberField.value,
+            shipping_address: shippingAddressField.value
+        }, token);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+window.addEventListener("load", fetchData);
+
+
+$updatebtn.addEventListener("click", update);
