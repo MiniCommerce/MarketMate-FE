a2bb89074ad382f6d5f7392375de263a1de1d5ea
allen9535 <allen9535@naver.com>
2023-09-02 20:24:36 +0900

    build: API 주소를 배포용으로 변경

--- src/js/data/urlDatas.js
+++ src/js/data/urlDatas.js
@@ -1,42 +1,42 @@
-export const loginURL = "http://127.0.0.1:8000/user/login/";
-export const buyerSignInURL = "http://127.0.0.1:8000/user/buyer/signin/";
-export const sellerSignInURL = "http://127.0.0.1:8000/user/seller/signin/";
-export const logoutURL = "http://127.0.0.1:8000/user/logout/";
-export const deleteUserURL = "http://127.0.0.1:8000/user/delete/";
-export const changePassWordURL = "http://127.0.0.1:8000/user/changepw/";
-export const buyerUpdateURL = "http://127.0.0.1:8000/user/buyer/update/";
-export const sellerUpdateURL = "http://127.0.0.1:8000/user/seller/update/";
-export const userDiscriminationURL = "http://127.0.0.1:8000/user/discrimination/"
+export const loginURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/login/";
+export const buyerSignInURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/buyer/signin/";
+export const sellerSignInURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/seller/signin/";
+export const logoutURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/logout/";
+export const deleteUserURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/delete/";
+export const changePassWordURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/changepw/";
+export const buyerUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/buyer/update/";
+export const sellerUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/seller/update/";
+export const userDiscriminationURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/discrimination/"
 
 // 상품 관련 URL
-export const productListURL = "http://127.0.0.1:8000/products/list/";
-export const productDetailURL = "http://127.0.0.1:8000/products/detail/";
-export const productRegisterURL = "http://127.0.0.1:8000/products/register/";
-export const productUpdateURL = "http://127.0.0.1:8000/products/update/";
-export const mysaleProduct = "http://127.0.0.1:8000/products/mysale/";
+export const productListURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/list/";
+export const productDetailURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/detail/";
+export const productRegisterURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/register/";
+export const productUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/update/";
+export const mysaleProduct = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/mysale/";
 
 // 장바구니 관련 URL
-export const cartAddURL = "http://127.0.0.1:8000/carts/add/";
-export const cartListURL = "http://127.0.0.1:8000/carts/list/";
-export const cartUpdateURL = "http://127.0.0.1:8000/carts/update/";
-export const cartDeleteURL = "http://127.0.0.1:8000/carts/delete/";
+export const cartAddURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/add/";
+export const cartListURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/list/";
+export const cartUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/update/";
+export const cartDeleteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/delete/";
 
 // 결제 관련 URL
-export const orderURL = "http://127.0.0.1:8000/purchases/order/";
-export const cartOrderURL = "http://127.0.0.1:8000/purchases/order/cart/";
-export const prepurchaseURL = "http://127.0.0.1:8000/purchases/prepurchase/";
-export const postpurchaseURL = "http://127.0.0.1:8000/purchases/postpurchase/";
-export const refundURL = "http://127.0.0.1:8000/purchases/refund/";
-export const buyerOrderURL = "http://127.0.0.1:8000/purchases/buyer_orders/";
+export const orderURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/order/";
+export const cartOrderURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/order/cart/";
+export const prepurchaseURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/prepurchase/";
+export const postpurchaseURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/postpurchase/";
+export const refundURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/refund/";
+export const buyerOrderURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/buyer_orders/";
 
 // 후기 관련 URL
-export const reviewlistURL = "http://127.0.0.1:8000/reviews/list/";
-export const reviewWriteURL = "http://127.0.0.1:8000/reviews/write/"
-export const reviewUpdateURL = "http://127.0.0.1:8000/reviews/update/"
-export const reviewDeleteURL = "http://127.0.0.1:8000/reviews/delete/"
+export const reviewlistURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/list/";
+export const reviewWriteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/write/"
+export const reviewUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/update/"
+export const reviewDeleteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/delete/"
 
 // 문의
-export const questionListURL = "http://127.0.0.1:8000/questions/list/";
-export const questionWriteURL = "http://127.0.0.1:8000/questions/write/";
-export const questionUpdateURL = "http://127.0.0.1:8000/questions/update/";
-export const questionDeleteURL = "http://127.0.0.1:8000/questions/delete/";
+export const questionListURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/list/";
+export const questionWriteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/write/";
+export const questionUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/update/";
+export const questionDeleteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/delete/";
