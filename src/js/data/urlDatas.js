9c77900f023307956c8527e2855d72c0b3bfbb09
allen9535 <allen9535@naver.com>
2023-09-03 09:07:06 +0900

    build: API 주소를 SSL 적용된 도메인으로 변경

--- src/js/data/urlDatas.js
+++ src/js/data/urlDatas.js
@@ -1,42 +1,42 @@
-export const loginURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/login/";
-export const buyerSignInURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/buyer/signin/";
-export const sellerSignInURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/seller/signin/";
-export const logoutURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/logout/";
-export const deleteUserURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/delete/";
-export const changePassWordURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/changepw/";
-export const buyerUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/buyer/update/";
-export const sellerUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/seller/update/";
-export const userDiscriminationURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/user/discrimination/"
+export const loginURL = "https://www.marketmate.digital/api/user/login/";
+export const buyerSignInURL = "https://www.marketmate.digital/api/user/buyer/signin/";
+export const sellerSignInURL = "https://www.marketmate.digital/api/user/seller/signin/";
+export const logoutURL = "https://www.marketmate.digital/api/user/logout/";
+export const deleteUserURL = "https://www.marketmate.digital/api/user/delete/";
+export const changePassWordURL = "https://www.marketmate.digital/api/user/changepw/";
+export const buyerUpdateURL = "https://www.marketmate.digital/api/user/buyer/update/";
+export const sellerUpdateURL = "https://www.marketmate.digital/api/user/seller/update/";
+export const userDiscriminationURL = "https://www.marketmate.digital/api/user/discrimination/"
 
 // 상품 관련 URL
-export const productListURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/list/";
-export const productDetailURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/detail/";
-export const productRegisterURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/register/";
-export const productUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/update/";
-export const mysaleProduct = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/products/mysale/";
+export const productListURL = "https://www.marketmate.digital/api/products/list/";
+export const productDetailURL = "https://www.marketmate.digital/api/products/detail/";
+export const productRegisterURL = "https://www.marketmate.digital/api/products/register/";
+export const productUpdateURL = "https://www.marketmate.digital/api/products/update/";
+export const mysaleProduct = "https://www.marketmate.digital/api/products/mysale/";
 
 // 장바구니 관련 URL
-export const cartAddURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/add/";
-export const cartListURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/list/";
-export const cartUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/update/";
-export const cartDeleteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/carts/delete/";
+export const cartAddURL = "https://www.marketmate.digital/api/carts/add/";
+export const cartListURL = "https://www.marketmate.digital/api/carts/list/";
+export const cartUpdateURL = "https://www.marketmate.digital/api/carts/update/";
+export const cartDeleteURL = "https://www.marketmate.digital/api/carts/delete/";
 
 // 결제 관련 URL
-export const orderURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/order/";
-export const cartOrderURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/order/cart/";
-export const prepurchaseURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/prepurchase/";
-export const postpurchaseURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/postpurchase/";
-export const refundURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/refund/";
-export const buyerOrderURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/purchases/buyer_orders/";
+export const orderURL = "https://www.marketmate.digital/api/purchases/order/";
+export const cartOrderURL = "https://www.marketmate.digital/api/purchases/order/cart/";
+export const prepurchaseURL = "https://www.marketmate.digital/api/purchases/prepurchase/";
+export const postpurchaseURL = "https://www.marketmate.digital/api/purchases/postpurchase/";
+export const refundURL = "https://www.marketmate.digital/api/purchases/refund/";
+export const buyerOrderURL = "https://www.marketmate.digital/api/purchases/buyer_orders/";
 
 // 후기 관련 URL
-export const reviewlistURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/list/";
-export const reviewWriteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/write/"
-export const reviewUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/update/"
-export const reviewDeleteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/reviews/delete/"
+export const reviewlistURL = "https://www.marketmate.digital/api/reviews/list/";
+export const reviewWriteURL = "https://www.marketmate.digital/api/reviews/write/"
+export const reviewUpdateURL = "https://www.marketmate.digital/api/reviews/update/"
+export const reviewDeleteURL = "https://www.marketmate.digital/api/reviews/delete/"
 
 // 문의
-export const questionListURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/list/";
-export const questionWriteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/write/";
-export const questionUpdateURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/update/";
-export const questionDeleteURL = "http://ec2-52-79-61-229.ap-northeast-2.compute.amazonaws.com/questions/delete/";
+export const questionListURL = "https://www.marketmate.digital/api/questions/list/";
+export const questionWriteURL = "https://www.marketmate.digital/api/questions/write/";
+export const questionUpdateURL = "https://www.marketmate.digital/api/questions/update/";
+export const questionDeleteURL = "https://www.marketmate.digital/api/questions/delete/";
