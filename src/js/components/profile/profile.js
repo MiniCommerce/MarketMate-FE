3b74e922d2397222a2e2cadafe805cbb97170547
allen9535 <allen9535@naver.com>
2023-09-04 18:53:22 +0900

    feat(profile): 프로필 페이지 수정

--- src/js/components/profile/profile.js
+++ src/js/components/profile/profile.js
@@ -1,6 +1,10 @@
 import { apiAuthGet } from "../../utils/fetchAPI.js";
 import { URL } from "../../data/index.js";
-import { getSessionStorage } from '../../utils/storage.js';
+import { getSessionStorage, setSessionStorage } from '../../utils/storage.js';
+
+const $updateBtn = document.querySelector("#update-btn");
+const $pwdUpdateBtn = document.querySelector("#pwd-update-btn");
+const $deleteBtn = document.querySelector("#delete-btn");
 
 const $buyerprofile = document.querySelector(".buyer-profile");
 const $sellerprofile = document.querySelector(".seller-profile");
@@ -15,9 +19,11 @@ async function fetchUserRole(event) {
         if (response.message === '판매자') {
             $buyerprofile.style.display = "none";
             $sellerprofile.style.display = "block";
+            setSessionStorage("type", "판매자");
         } else if (response.message === '구매자') {
             $buyerprofile.style.display = "block";
             $sellerprofile.style.display = "none";
+            setSessionStorage("type", "구매자");
         }
     } catch (err) {
         console.log(err);
@@ -25,3 +31,22 @@ async function fetchUserRole(event) {
 }
 
 window.addEventListener("load", fetchUserRole);
+$updateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (getSessionStorage("type") === "구매자") {
+        location.href = "profile_update.html";
+    } else {
+        location.href = "seller_profile_update.html";
+    }
+});
+$pwdUpdateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    location.href = "change_pw.html";
+});
+$deleteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    location.href = "user_delete.html";
+})
